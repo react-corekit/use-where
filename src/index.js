d2592ac08557b01c791e30c8b15f6797af5391f6
glongh <glongh@gmail.com>
2020-01-02 19:14:24 -0500

    feat(*): Return array with methods

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 
+/**
+  Provides
+ */
 export const useWhere = () => {
+  // Returns true if there is a browser's window object.
   const isBrowser = () =>
     typeof window !== "undefined" &&
     {}.toString.call(window) === "[object Window]";
+  // Returns true if there is a node's global object.
   const isNode = () =>
     typeof global !== "undefined" &&
     {}.toString.call(global) === "[object global]";
   const isServer = () => !isBrowser();
 
-  return () => [isBrowser, isNode, isServer];
+  return [isBrowser, isNode, isServer];
 };
